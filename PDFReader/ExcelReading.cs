c01ca56a1dea9cae8b9096a62063f453b154abb4
Phushyamithra <60391390+Phushyamithra@users.noreply.github.com>
2021-06-05 22:24:40 +0530

    Merge two pdfs

---
+++ PDFReader/ExcelReading.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+
+namespace PDFReader
+{
+    class ExcelReading
+    {
+        
+        public static void Main(String [] args)
+        {
+            string path1 = "E:\\INTADM21DF006.xlsx";
+            var excelFIle = new Application();
+            Workbook wb = excelFIle.Workbooks.Open(path1);
+            int sheetNo = 1;
+            Worksheet ws = wb.Worksheets[sheetNo];
+            Console.ReadLine();
+        }
+    }
+}
